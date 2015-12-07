bde6531e0415cade463d93b3409cf8389e814d2d
lbob <skyweo@foxmail.com>
2015-12-07 17:26:38 +0800

    Add AuthStorage

---
+++ src/Authentication/AuthStorage.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lbob
+ * Date: 2015/12/7
+ * Time: 17:23
+ */
+
+namespace Xaircraft\Authentication;
+
+
+use Xaircraft\Authentication\Contract\CurrentUser;
+use Xaircraft\Web\Session;
+
+class AuthStorage
+{
+    private static $currentUserSessionID = '__current_user__';
+
+    public static function set(CurrentUser $user)
+    {
+        Session::put(self::$currentUserSessionID, $user);
+    }
+
+    public static function get()
+    {
+        return Session::get(self::$currentUserSessionID);
+    }
+}
