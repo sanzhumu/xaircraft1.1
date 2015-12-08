2d5b43faae3e589a15cc5aad768a98017e604e70
lbob <skyweo@foxmail.com>
2015-12-08 10:24:15 +0800

    修复一个DI的BUG，但此BUG在之前是不存在的，应该是不小心删除了一个else单词所致。

--- src/Web/Mvc/Attribute/AuthorizeAttribute.php
+++ src/Web/Mvc/Attribute/AuthorizeAttribute.php
@@ -37,10 +37,15 @@ class AuthorizeAttribute extends Attribute
         $items = explode(',', $source);
         if (!empty($items)) {
             foreach ($items as $item) {
-                if (preg_match('#(?<key>[a-zA-Z][a-zA-Z0-9\_]+)\=[\'\"]?(?<value>[^\'\"\,]+)[\'\"]?#i', $item, $matches)) {
-                    $arguments[$matches['key']] = $matches['value'];
-                } else {
-                    throw new AttributeException("Invalid attribute arguments.");
+                $pairs = explode(',', $item);
+                if (!empty($pairs)) {
+                    foreach ($pairs as $pair) {
+                        if (preg_match('#(?<key>[a-zA-Z][a-zA-Z0-9\_]+)\=[\'\"]?(?<value>[^\'\"]+)[\'\"]?#i', $pair, $matches)) {
+                            $arguments[$matches['key']] = trim($matches['value']);
+                        } else {
+                            throw new AttributeException("Invalid attribute arguments.");
+                        }
+                    }
                 }
             }
         }
