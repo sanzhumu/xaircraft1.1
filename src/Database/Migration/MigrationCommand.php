ab5a487a3e749859f67665a6e32285784556e6f1
lbob <skyweo@foxmail.com>
2015-12-14 14:27:34 +0800

    MigrationCommand增加--history和--forget两个命令

--- src/Database/Migration/MigrationCommand.php
+++ src/Database/Migration/MigrationCommand.php
@@ -10,6 +10,7 @@ namespace Xaircraft\Database\Migration;
 
 
 use Xaircraft\App;
+use Xaircraft\Configuration\Settings;
 use Xaircraft\Console\Command;
 use Xaircraft\Console\Console;
 use Xaircraft\Core\IO\File;
@@ -18,6 +19,7 @@ use Xaircraft\Exception\ConsoleException;
 
 class MigrationCommand extends Command
 {
+    private $history;
 
     public function handle()
     {
@@ -26,9 +28,15 @@ class MigrationCommand extends Command
                 case "--make":
                     $this->makeMigration();
                     return;
+                case "--history":
+                    $this->historyMigration();
+                    return;
+                case "--forget":
+                    $this->forgetMigration();
+                    return;
             }
         }
-        throw new ConsoleException("Please input migration command arguments: [--make].");
+        throw new ConsoleException("Please input migration command arguments: [--make][--history].");
     }
 
     private function makeMigration()
@@ -37,11 +45,69 @@ class MigrationCommand extends Command
             throw new ConsoleException("Invalid migration argument in [--make].");
         }
         $migration = $this->args[1];
-        $author = isset($this->args[2]) ? $this->args[2] : "Unknow";
+        $author = isset($this->args[2]) ? $this->args[2] : "Unknown";
         $class = 'm' . time() . Strings::snakeToCamel($migration);
         $path = App::path('migration') . "/$class.php";
         File::writeText($path, Template::generate($class, $author));
 
         Console::line("migration [$migration] finished in [$class.php].");
+    }
+
+    private function historyMigration()
+    {
+        $history = Settings::get(App::path('migration_history'));
+        if (isset($history)) {
+            $history = unserialize($history);
+
+            if (!empty($history)) {
+                $this->history = $history;
+                $date = "Unknown datetime";
+                foreach ($this->history as $key => $value) {
+                    if (preg_match('#^m(?<timestamp>\d+)#i', $value, $match)) {
+                        if (array_key_exists('timestamp', $match)) {
+                            $timestamp = intval($match['timestamp']);
+                            $date = date("Y-m-d H:i:s", $timestamp);
+                        }
+                    }
+                    Console::line("$key.[$date][$value]");
+                }
+            }
+        }
+    }
+
+    private function forgetMigration()
+    {
+        if (!isset($this->args[1])) {
+            throw new ConsoleException("Invalid migration argument in [--forget].");
+        }
+
+        $selectedIndex = intval($this->args[1]);
+
+        $history = Settings::get(App::path('migration_history'));
+        if (isset($history)) {
+            $history = unserialize($history);
+
+            if (!empty($history)) {
+                $this->history = $history;
+                $date = "Unknown datetime";
+                foreach ($this->history as $key => $value) {
+                    if ($key === $selectedIndex) {
+                        unset($this->history[$selectedIndex]);
+                        if (preg_match('#^m(?<timestamp>\d+)#i', $value, $match)) {
+                            if (array_key_exists('timestamp', $match)) {
+                                $timestamp = intval($match['timestamp']);
+                                $date = date("Y-m-d H:i:s", $timestamp);
+                            }
+                        }
+                        Settings::save(App::path('migration_history'), serialize($this->history));
+                        Console::line("$key.[$date][$value] has forget.");
+                        return;
+                    }
+                }
+                Console::line("Can't find index [$selectedIndex].");
+                return;
+            }
+        }
+        Console::line("History is empty.");
     }
 }
