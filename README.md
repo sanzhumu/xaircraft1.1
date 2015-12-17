56aa5da6b385cb43175ad00231c5521b2fed43c4
lbob <skyweo@foxmail.com>
2015-12-17 20:35:07 +0800

    修改文档

--- README.md
+++ README.md
@@ -3,11 +3,8 @@ PHP framework 1.1 for Xaircraft Co.
 
 ## 目录
 
-1. [Action 参数注入](https://github.com/lbob/xaircraft1.1/wiki/Action)
+1. [Action](https://github.com/lbob/xaircraft1.1/wiki/Action)
+    - 1.1 Action 参数注入
+    - 1.2 Action 异常输出控制
 2. [Json 工具类](https://github.com/lbob/xaircraft1.1/wiki/JSON-%E5%B7%A5%E5%85%B7%E7%B1%BB)
 3. [依赖注入和控制反转](https://github.com/lbob/xaircraft1.1/wiki/%E4%BE%9D%E8%B5%96%E6%B3%A8%E5%85%A5%E5%92%8C%E6%8E%A7%E5%88%B6%E5%8F%8D%E8%BD%AC)
-4. Action 异常输出控制
-
-
-
-
--- app/controllers/user/home.php
+++ app/controllers/user/home.php
@@ -3,54 +3,12 @@ use Xaircraft\DI;
 use Xaircraft\Web\Mvc\Controller;
 
 /**
- * Created by PhpStorm.
- * User: lbob
- * Date: 2015/11/12
- * Time: 17:44
+ * @output_status_exception
  */
 class user_home_controller extends Controller
 {
-    /**
-     * @var Message
-     */
-    private $message;
-
-    public function __construct(Message $message)
+    public function test_error()
     {
-        $this->message = $message;
-    }
-
-    public function index()
-    {
-        var_dump($this->message);
-        $this->message->sendEmail("Bob");
-    }
-
-    public function test_message()
-    {
-        $message1 = DI::get(Message::class);
-        $message1->id = 2;
-        $message2 = DI::get(Message::class);
-        $message2->id = 3;
-
-        var_dump($message1);
-        var_dump($message2);
-    }
-
-    public function test_pluck()
-    {
-        var_dump(\Xaircraft\DB::table('user')->pluck('id')->where('id', 23423)->execute());
-    }
-
-    public function test_multi_database()
-    {
-        //var_dump(\Xaircraft\DB::getDatabaseName());
-        $user = \Xaircraft\DB::entity('user');
-        var_dump($user);
-
-        \Xaircraft\DB::database('agri_data_center');
-        //var_dump(\Xaircraft\DB::getDatabaseName());
-        $user = \Xaircraft\DB::entity('user');
-        var_dump($user);
+        throw new \Exception("test error");
     }
 }
